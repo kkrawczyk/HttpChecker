0b1cf88fe6c1695b84bc2ac31f20acdef942fb02
Krzysztof Krawczyk <k.krawczyk@nova-it.eu>
2013-11-22 17:03:17 +0100

    New version prepared to use with TDD

---
+++ src/main/java/eu/novait/httpchecker/App.java
@@ -0,0 +1,13 @@
+package eu.novait.httpchecker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
